b0a3b076208a54dbb0e3e19baebc0aa060de1b8a
Sami @JUNIORCO <sami.juniorco@gmail.com>
2024-09-05 16:29:17 -0400

    pricing page more changes

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -3,13 +3,13 @@ import Routes from "@/routes";
 import { currentUser } from "@clerk/nextjs/server";
 import { InfoIcon } from "lucide-react";
 import Link from "next/link";
-import type { FC } from "react";
+import type { FC, ReactNode } from "react";
 
 export default async function PricingPage() {
   const user = await currentUser();
 
   const HelperBtn: FC<{
-    content: string;
+    content: ReactNode;
   }> = ({ content }) => {
     return (
       <div className="dropdown dropdown-end">
@@ -25,8 +25,7 @@ export default async function PricingPage() {
           className="card compact dropdown-content bg-base-100 rounded-box z-[1] w-64 shadow"
         >
           <div tabIndex={0} className="card-body">
-            <h2 className="card-title">Here's the math</h2>
-            <p>{content}</p>
+            {content}
           </div>
         </div>
       </div>
@@ -36,12 +35,15 @@ export default async function PricingPage() {
   return (
     <PageContentContainer>
       <div className="flex flex-col gap-4 justify-center items-center">
-        <p className="text-xl font-semibold">Simple, Easy, Flexible</p>
-        <p className="text-4xl font-bold my-6">$0.005 per run</p>
+        <p className="text-xl font-semibold">Simple, Affordable, Flexible</p>
+        <div className="flex flex-col gap-2 my-6 justify-center items-center">
+          <p className="text-2xl font-bold">100 free runs per month, then</p>
+          <p className="text-4xl font-bold">$0.005 per run</p>
+        </div>
         {!user ? (
           <Link href={Routes.signUp}>
-            <button type="button" className="btn btn-primary btn-lg">
-              Get Started Now!
+            <button type="button" className="btn btn-primary btn-lg btn-wide">
+              Get Started For Free
             </button>
           </Link>
         ) : null}
@@ -50,14 +52,24 @@ export default async function PricingPage() {
       <div className="flex flex-col gap-4">
         <Title>Estimate Your Usage</Title>
 
-        <div className="grid grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
           <div className="card bg-base-100 shadow-md">
             <div className="card-body">
               <p className="font-bold">Low Usage</p>
               <p>2 Pyngs, each runs every 4 hours</p>
               <h2 className="card-title" style={{ gap: "0px" }}>
-                $1.8 per month
-                <HelperBtn content="2 Pyngs &times; 6 times a day &times; 30 days &times; 0.005 / run = $1.8 / mo" />
+                $1.75 per month
+                <HelperBtn
+                  content={
+                    <p>
+                      2 Pyngs <br />
+                      &times; 6 times a day <br />
+                      &times; 30 days <br />
+                      &times; $0.005 / run <br />
+                      &minus; 100 free runs ($0.5) <br />= $1.75 / mo
+                    </p>
+                  }
+                />
               </h2>
             </div>
           </div>
@@ -68,8 +80,18 @@ export default async function PricingPage() {
               <p>10 Pyngs</p>
               <p>5 run every hour, 5 run every 15 mins</p>
               <h2 className="card-title" style={{ gap: "0px" }}>
-                $90 per month
-                <HelperBtn content="(5 Pyngs &times; 24 times a day &times; 30 days &times; 0.005 / run) + (5 Pyngs &times; 96 times a day &times; 30 days &times; 0.005 / run) = $90 / mo" />
+                $89.5 per month
+                <HelperBtn
+                  content={
+                    <p>
+                      (5 Pyngs &times; 24 times a day <br />+ 5 Pyngs &times; 96
+                      times a day) <br />
+                      &times; 30 days <br />
+                      &times; $0.005 / run <br />
+                      &minus; 100 free runs ($0.5) <br />= $89.5 / mo
+                    </p>
+                  }
+                />
               </h2>
             </div>
           </div>
@@ -79,18 +101,28 @@ export default async function PricingPage() {
               <p className="font-bold">High Usage</p>
               <p>25 Pyngs, each runs every 15 mins</p>
               <h2 className="card-title" style={{ gap: "0px" }}>
-                $360 / month
-                <HelperBtn content="25 Pyngs &times; 96 times a day &times; 30 days &times; 0.005 / run = $360 / mo" />
+                $359.5 / month
+                <HelperBtn
+                  content={
+                    <p>
+                      25 Pyngs <br />
+                      &times; 96 times a day <br />
+                      &times; 30 days <br />
+                      &times; $0.005 / run <br />
+                      &minus; 100 free runs ($0.5) <br />= $359.5 / mo
+                    </p>
+                  }
+                />
               </h2>
             </div>
           </div>
         </div>
       </div>
 
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 mb-8">
         <Title>FAQ</Title>
-        <div className="flex flex-col gap-2">
-          <div className="collapse collapse-arrow bg-base-100 shadow-md">
+        <div className="flex flex-col gap-4">
+          <div className="collapse collapse-arrow bg-base-200 shadow-md">
             <input type="radio" name="my-accordion-2" defaultChecked />
             <div className="collapse-title text-xl font-medium">
               What is a Pyng and a run?
@@ -103,7 +135,7 @@ export default async function PricingPage() {
             </div>
           </div>
 
-          <div className="collapse collapse-arrow bg-base-100 shadow-md">
+          <div className="collapse collapse-arrow bg-base-200 shadow-md">
             <input type="radio" name="my-accordion-2" />
             <div className="collapse-title text-xl font-medium">
               Can I delete a Pyng anytime?
@@ -118,7 +150,7 @@ export default async function PricingPage() {
             </div>
           </div>
 
-          <div className="collapse collapse-arrow bg-base-100 shadow-md">
+          <div className="collapse collapse-arrow bg-base-200 shadow-md">
             <input type="radio" name="my-accordion-2" />
             <div className="collapse-title text-xl font-medium">
               Is there a free tier?
@@ -128,7 +160,7 @@ export default async function PricingPage() {
             </div>
           </div>
 
-          <div className="collapse collapse-arrow bg-base-100 shadow-md">
+          <div className="collapse collapse-arrow bg-base-200 shadow-md">
             <input type="radio" name="my-accordion-2" />
             <div className="collapse-title text-xl font-medium">
               What is the billing cycle?
